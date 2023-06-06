b5beee7fe331566528b16575a70e60e44040ca2b
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2023-06-06 08:43:59 +0600

    Header and nav bar is completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      {/* Header */}
+      <Header></Header>
+      <Outlet></Outlet>
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+import Books from "./components/Books.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "books",
+        element: <Books />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+
+  // error page
+  {
+    path: "/",
+    element: <ErrorPage></ErrorPage>,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
